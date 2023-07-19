e71206aeaf808003d4b9522197be154e4fddf710
Scolage <138793552+Scolage@users.noreply.github.com>
2023-07-19 20:44:59 -0300

    pequena mudança

--- jogos/21.rb
+++ jogos/21.rb
@@ -2,7 +2,7 @@ def total?()
    
 end
 puts "Bem Vindo(a) ao 21!!!"
-baralho = [2,3,4,5,6,7,8,9,10,'J','K','Q','A']
+baralho = ['2','3','4','5','6','7','8','9','10','J','K','Q','A']
 baralho *= 4
 baralho.shuffle!
 
